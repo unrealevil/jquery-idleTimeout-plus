bae8c83e719721df952c75e4dd51c59a0364b945
CrashSensei <crashsensei@linearsoft.com>
2016-09-16 10:43:37 -0600

    Cleaned up ESlint warnings in dist code

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 var gulp = require('gulp');
 var stripCode = require('gulp-strip-code');
 var stripLine = require('gulp-strip-line');
@@ -7,62 +8,70 @@ var uglify = require('gulp-uglify');
 var del = require('del');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('clean', function () {
-    del('dist/js/*')
-    return del('dist/css/*');
+gulp.task('clean', function() {
+  del('dist/js/*');
+  return del('dist/css/*');
 });
 
-gulp.task('standard', ['clean'], function () {
-   return gulp.src(['src/jquery-idleTimeout-plus.js'])
-        .pipe(stripCode({
-            start_comment: '--strip_testing_begin--',
-            end_comment: '--strip_testing_end--'
-        }))
-        .pipe(stripCode({
-            start_comment: '--strip_iframe_begin--',
-            end_comment: '--strip_iframe_end--'
-        }))
-        .pipe(stripLine([
-            /--strip_testing--/,
-            /--strip_iframe--/,
-            /console.log/
-        ]))
-        .pipe(gulp.dest('dist/js'));
+gulp.task('standard', ['clean'], function() {
+  return gulp.src(['src/jquery-idleTimeout-plus.js'])
+    .pipe(stripCode({
+      start_comment: '--strip_testing_begin--',
+      end_comment: '--strip_testing_end--'
+    }))
+    .pipe(stripCode({
+      start_comment: '--strip_iframe_begin--',
+      end_comment: '--strip_iframe_end--'
+    }))
+    .pipe(stripLine([
+      /--strip_testing--/,
+      /--strip_iframe--/,
+      /console.log/,
+      /@namespace/,
+      /eslint-disable-next-line/
+    ]))
+    .pipe(replace(/({|;)\s*\n\n+(\s*)/g,'$1\n$2'))
+    .pipe(replace(/({|;)(\s+.*)\n\n+(\s*)/g,'$1$2\n$3'))
+    .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('iframe',['clean','standard'],  function () {
-    return gulp.src(['src/jquery-idleTimeout-plus.js'])
-        .pipe(stripCode({
-            start_comment: '--strip_testing_begin--',
-            end_comment: '--strip_testing_end--'
-        }))
-        .pipe(stripLine([
-            /--strip_testing--/,
-            /--strip_iframe_/,
-            /console.log/
-        ]))
-        .pipe(replace(/\/\* --strip_iframe--.*/g,''))
-        .pipe(rename('jquery-idleTimeout-plus-iframe.js'))
-        .pipe(gulp.dest('dist/js'));
+gulp.task('iframe', ['clean', 'standard'], function() {
+  return gulp.src(['src/jquery-idleTimeout-plus.js'])
+    .pipe(stripCode({
+      start_comment: '--strip_testing_begin--',
+      end_comment: '--strip_testing_end--'
+    }))
+    .pipe(stripLine([
+      /--strip_testing--/,
+      /--strip_iframe_/,
+      /console.log/,
+      /@namespace/,
+      /eslint-disable-next-line/
+    ]))
+    .pipe(replace(/\s\/\* --strip_iframe--.*/g, ''))
+    .pipe(replace(/({|;)\s*\n\n+(\s*)/g,'$1\n$2'))
+    .pipe(replace(/({|;)(\s+.*)\n\n+(\s*)/g,'$1$2\n$3'))
+    .pipe(rename('jquery-idleTimeout-plus-iframe.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('mini', ['clean','standard','iframe'], function () {
-    return gulp.src(['dist/js/*.js'])
-        .pipe(uglify())
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest('dist/js'))
+gulp.task('mini', ['clean', 'standard', 'iframe'], function() {
+  return gulp.src(['dist/js/*.js'])
+    .pipe(uglify())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('css', ['clean'], function () {
-    return gulp.src(['src/*.css'])
-        .pipe(gulp.dest('dist/css'))
+gulp.task('css', ['clean'], function() {
+  return gulp.src(['src/*.css'])
+    .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('minicss', ['clean','css'], function () {
-    return gulp.src(['dist/css/*.css'])
-        .pipe(cleanCSS())
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest('dist/css'))
+gulp.task('minicss', ['clean', 'css'], function() {
+  return gulp.src(['dist/css/*.css'])
+    .pipe(cleanCSS())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('default', ['clean','standard','iframe','mini','css','minicss']);
+gulp.task('default', ['clean', 'standard', 'iframe', 'mini', 'css', 'minicss']);
